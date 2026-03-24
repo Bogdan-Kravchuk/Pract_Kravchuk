f9a14626766b12336bc92f4c07439e21a3cfb9ce
Bohdan Kravchuk <bogdankravchuk.game@gmail.com>
2026-03-24 13:18:36 +0200

    Pract10 4 commit

--- src/main/java/Pacts/Pract10_Kravchuk/EventLab.java
+++ src/main/java/Pacts/Pract10_Kravchuk/EventLab.java
@@ -10,8 +10,33 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class EventLab {
+    public static List<Event> pick(List<Event> events, Predicate<Event> condition) {
+        List<Event> result = new ArrayList<>();
+        for (Event e : events) {
+            if (condition.test(e)) {
+                result.add(e);
+            }
+        }
+        return result;
+    }
 
+    public static List<String> labels(List<Event> events, Function<Event, String> formatter) {
+        List<String> result = new ArrayList<>();
+        for (Event e : events) {
+            result.add(formatter.apply(e));
+        }
+        return result;
+    }
 
+    public static void notifyAll(List<Event> events, Consumer<Event> action) {
+        for (Event e : events) {
+            action.accept(e);
+        }
+    }
+
+    public static Event create(Supplier<Event> factory) {
+        return factory.get();
+    }
     public static void findConflicts(List<Event> e){
 
         for(int i =0; i < e.size(); i++){
